789a764177ae4245a1423c5ecefe4a22da30e698
golem84 <andr0073@yandex.ru>
2022-09-05 22:55:52 +0500

    Revert "add some methods to DFrame"
    This reverts commit b864d8459b5698c516216030552318e3c438e013.

--- DFrameLib/DFrame.cs
+++ DFrameLib/DFrame.cs
@@ -1,44 +1,9 @@
 ﻿using System.Data;
-// нельзя в библиотеку добавить ссылку для использования COM объектов.
 
 namespace DFrameLib
 {
     public class DFrame : DataTable
     {
-        // добавить колонку (именованная типизированная, именованная с массивом данных)
-        //public void AddCol(string _name) => this.Columns.Add(_name);
-        public void AddCol(string _name, Type _type) => this.Columns.Add(_name, _type);
-        public void AddCol(string _name, object[] item)
-        {
-            this.Columns.Add(_name);
-            this.AddItemToCol(_name, item);
-        }
-        
-        public void AddItemToCol(string colname, object[] item) // добавить item в столбец colname
-        {
-            DataRow newrow;
-            for (int i = 0; i < item.Length; i++)
-            {
-                newrow = this.NewRow();
-                try
-                {
-                    newrow[colname] = item[i];
-                    this.Rows.Add(newrow);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Ошибка. ", ex.Message);
-                    return ;
-                }
-            }
-        }
-
-        // Добавляем массив объектов как новую строку в таблицу
-        public void AddRow(object[] row ) => this.Rows.Add(row); 
-        
-        
-
-
 
     }
 }
