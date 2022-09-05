4c13be66f2310fb0f6f93c8e351e6cad0fbe0504
golem84 <andr0073@yandex.ru>
2022-09-05 22:58:45 +0500

    Add some methods to DFrame class

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,20 +1,18 @@
 ﻿using DFrameLib;
+//using Microsoft.Office.Tools.Excel;
+using Microsoft.Office.Interop.Excel;
 using System.Data;
 using Excel = Microsoft.Office.Interop.Excel;
-//using Microsoft.Office.Tools.Excel;
-using System.Globalization;
-using Microsoft.Office.Interop.Excel;
-using System.Reflection;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
         var df = new DFrame();
-
+        // подключаемся к Excel
         Application exApp = new Application();
-        System.Diagnostics.Process excelProc = System.Diagnostics.Process.GetProcessesByName("EXCEL").Last();
-
+        System.Diagnostics.Process excelProc = 
+            System.Diagnostics.Process.GetProcessesByName("EXCEL").Last();
         if (exApp == null)
         {
             Console.WriteLine("Excel is not installed!");
@@ -23,87 +21,48 @@ internal class Program
         // файл находится в одной папке с программой
         string path = Path.GetDirectoryName(typeof(Program).Assembly.Location);
         // настраиваем переменные для работы с Excel
-        
-            Excel.Workbook wb = exApp.Workbooks.Open(path + "\\Book1.xlsx");
-            Excel.Worksheet ws = wb.Sheets[1];
-            int maxrow = ws.UsedRange.Rows.Count;
-            int maxcol = ws.UsedRange.Columns.Count;
-        
+        Workbook wb = exApp.Workbooks.Open(path + @"\Book1.xlsx");
+        Worksheet ws = wb.Sheets[1]; // нумерация листов начинается с 1
+        int maxrow = ws.UsedRange.Rows.Count;
+        int maxcol = ws.UsedRange.Columns.Count;
 
-        Console.WriteLine("reading column names.");
+        Console.WriteLine("Создаем колонки, присваиваем тип данных.");
         //List<string> colnames = new List<string>();
-        for (int i = 1; i <= maxcol; i++)
-        {
-            df.Columns.Add(new DataColumn(ws.Cells[1, i].Value.ToString(), ws.Cells[2, i].Value.GetType()));            
-            //df.Columns[i-1].DataType = ws.Cells[i, 2].Value.GetType();
-        }
+        for (int i = 1; i <= maxcol; i++) 
+            df.AddCol(ws.Cells[1, i].Value.ToString(), 
+                ws.Cells[2, i].Value.GetType());
+        Console.WriteLine("Читаем данные в объект DFrame.");
 
         object[] row = new object[maxcol];
         for (int j = 2; j <= maxrow; j++)
         {
-            for (int i=1; i <= maxcol; i++)
+            for (int i = 1; i <= maxcol; i++)
             {
-                row[i-1]=(ws.Cells[j, i].Value);
+                row[i - 1] = ws.Cells[j, i].Value;
             }
-            df.Rows.Add(row);
-            row.Initialize();
+            //df.Rows.Add(row);
+            df.AddRow(row);
         }
-        /*
-        foreach(DataColumn c in df.Columns)
-        {
-            Console.WriteLine(c.DataType.ToString());
-        }
-        */
+        
         Console.WriteLine("Вывод всех столбцов без заголовков:");
         var items = df.Select();
-        foreach (var b in items) Console.WriteLine("{0}\t{1}\t{2}", b["id"],b["Name"], b["Age"]);
-        //Console.WriteLine();
-
+        foreach (var b in items) Console.WriteLine("{0}\t{1}\t{2}", 
+            b["id"], b["Age"], b["Name"]);
+        
         Console.WriteLine("Выбор строк, где Age >= 24:");
         string expr = "Age >= 24";
         DataRow[] foundRows = df.Select(expr);
-        for (int i = 0; i < foundRows.Length; i++)
-        {
-            Console.WriteLine(foundRows[i][0]+"\t"+ foundRows[i][1]);
-        }
+        for (int i = 0; i < foundRows.Length; i++) 
+            Console.WriteLine($"{foundRows[i][0]}\t{foundRows[i][1]}");
+        
         Console.WriteLine("Выбор строк, где Name = 'Ann':");
         expr = "Name = 'Ann'";
         DataRow[] foundRows2 = df.Select(expr);
         for (int i = 0; i < foundRows2.Length; i++)
-        {
-            Console.WriteLine(foundRows2[i][0] + "\t" + foundRows2[i][1] + "\t" + foundRows2[i][2]);
-        }
+            Console.WriteLine(foundRows2[i][0] + "\t" + 
+                foundRows2[i][1] + "\t" + foundRows2[i][2]);
+       
 
-        //Console.WriteLine(df.Columns.Count);
-
-        //foreach (string s in colnames) Console.Write(s + "\t");
-
-
-
-        /*
-        for ( int i =1; i<=maxrow; i++)
-        {
-            for (int j = 1; j <=maxcol; j++)
-            {
-                string s = ws.Cells[i, j].Value.ToString();
-                //string s = range.Text;
-                Console.Write(s+"\t");
-            }
-            Console.WriteLine();
-        }
-        
-        var t = ws.Cells[1, 1].Value.GetType();
-        Console.WriteLine("Cell[1,1] has '"+t+"' format.");
-
-        t = ws.Cells[2, 2].Value.GetType();
-        Console.WriteLine("Cell[2,2] has '" + t + "' format.");
-
-        t = ws.Cells[3, 1].Value.GetType();
-        Console.WriteLine("Cell[3,1] has '" + t + "' format.");
-        /*
-        var df = new DFrame();
-        var col = new DataColumn("Id", Type.GetType("System.Int32"));
-        */
 
 
 
